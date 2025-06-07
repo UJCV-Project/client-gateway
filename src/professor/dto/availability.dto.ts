866d9883e8160687cf403b83f62482fc60178d82
Marcio Maradiaga <mmmmaradiaga@outlook.com>
2025-06-06 19:51:39 -0600

    feature: disponibilidad de horarios

---
+++ src/professor/dto/availability.dto.ts
@@ -0,0 +1,27 @@
+import { Type } from 'class-transformer';
+import { ValidateNested, IsArray, IsNotEmpty, IsString } from 'class-validator';
+import { IsInt, Min, Max } from 'class-validator';
+
+export class AvailabilityDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => AvailableSlot)
+  slots: AvailableSlot[];
+}
+
+export class AvailableSlot {
+  @IsInt()
+  @Min(0)
+  @Max(6)
+  dayOfWeek: number;
+
+  @IsInt()
+  @Min(0)
+  @Max(2359)
+  startTime: number;
+
+  @IsInt()
+  @Min(0)
+  @Max(2359)
+  endTime: number;
+}
--- src/professor/professor.controller.ts
+++ src/professor/professor.controller.ts
@@ -1,19 +1,23 @@
-import { Body, Controller, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Logger, Param, Patch, Post, Query } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { PaginationDto } from 'src/common';
 import { PROFESSOR_SERVICE } from 'src/config';
 import { CreateProfessorDto, UpdateProfessorDto } from './dto';
+import { AvailabilityDto } from './dto/availability.dto';
 
 @Controller('professor')
 export class ProfessorController {
     constructor(
         @Inject(PROFESSOR_SERVICE) private readonly client: ClientProxy,
-    ) {}
+    ) { }
+    logger = new Logger('Professor Service');
+
+    //Professor
 
     @Post()
     async createProfessor(@Body() createProfessor: CreateProfessorDto) {
-    const response = await this.client.send({ cmd: 'createProfessor' }, createProfessor);
-    return response;
+        const response = await this.client.send({ cmd: 'createProfessor' }, createProfessor);
+        return response;
     }
 
     @Get()
@@ -34,12 +38,27 @@ export class ProfessorController {
     }
 
     @Patch(':code')
-    updateProfessorByCode(@Param('code') code: string, @Body() updateProfessorDto:UpdateProfessorDto) {
+    updateProfessorByCode(@Param('code') code: string, @Body() updateProfessorDto: UpdateProfessorDto) {
         console.log(code);
         return this.client.send(
             { cmd: 'updateProfessorByCode' },
-            {code,data: updateProfessorDto},
+            { code, data: updateProfessorDto },
         );
     }
 
+    //AvailableSchedule
+    @Post('/:id/available/')
+    async saveAvailabilitySchedule(@Param('id') professorId: string, @Body() slots: AvailabilityDto) {
+        const payload =  { professorId, ...slots }
+        this.logger.debug(payload);
+        const response = await this.client.send({ cmd: 'saveAvailabilitySchedule' }, payload);
+        return response;
+    }
+
+    @Get('/:id/available/')
+    async findAvailabilitySchedule(@Param('id') professorId: string) {
+        this.logger.debug(`Professor ID ${professorId}`);
+        const response = await this.client.send({ cmd: 'findAvailabilityById' }, professorId);
+        return response;
+    }
 }
